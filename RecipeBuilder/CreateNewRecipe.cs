a6fb58fa81dbb129198b3a8c2e2e2c3f23d92f47
bdpetersen77 <48611421+bdpetersen77@users.noreply.github.com>
2020-10-22 23:03:28 -0600

    Test comment

--- RecipeBuilder/CreateNewRecipe.cs
+++ RecipeBuilder/CreateNewRecipe.cs
@@ -17,6 +17,7 @@ namespace RecipeBuilder
             InitializeComponent();
         }
 
+       //Test comment for commit
         private void EntryRecipeName_TextChanged(object sender, EventArgs e)
         {
 
